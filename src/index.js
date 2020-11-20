782c33602545fbe5e17a72fdb15579629ced013e
Tony Nie <tony.nie@rnbtechgroup.com>
2020-11-21 00:34:03 +0800

    Fix issue that tempcalc.js not pushed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Game from './ttt.js'
 import Clock from './clock.js'
 import LoginControl from './login.js'
 import Form from './form.js'
-import Calculator from './tempcalc.js'
+// import Calculator from './tempcalc.js'
 
 // ========================================
 
@@ -14,7 +14,6 @@ ReactDOM.render(
             <LoginControl />
             <Clock />
             <Form />
-            <Calculator />
             <Game />
         </div>
     ),
